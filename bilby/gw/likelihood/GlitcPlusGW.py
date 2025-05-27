5c8de7abacfd8cbe82a0beacc00b834e38bb3cd2
Sharan <sharan.banagiri@ligo.org>
2025-05-26 21:12:25 -0700

    glitch likelihood model with erf glitch

--- bilby/gw/likelihood/GlitcPlusGW.py
+++ bilby/gw/likelihood/GlitcPlusGW.py
@@ -28,7 +28,7 @@ class GlitchIncludedGravitationalWaveTransient(GravitationalWaveTransient):
         jitter_time=True,
         reference_frame="sky",
         time_reference="geocenter",
-        ifo_with_glitch=None,
+        ifos_with_glitch=None,
     ):
 
         super(GlitchIncludedGravitationalWaveTransient, self).__init__(
@@ -44,7 +44,17 @@ class GlitchIncludedGravitationalWaveTransient(GravitationalWaveTransient):
             time_reference=time_reference,
         )
 
-        self.ifo_with_glitch = ifo_with_glitch
+        self.ifos_with_glitch = ifos_with_glitch
+
+    @classmethod
+    def frequency_domain_wavelet(A, t0, sigma, frequencies):
+
+        wavelet_term1 = A * 1j / (2 * np.pi * frequencies) * np.exp(-2 * np.pi * 1j * frequencies * t0 ) 
+        wavelet_term2 = np.exp( - (np.pi * frequencies * sigma)**2)
+
+        return wavelet_term1 * wavelet_term2
+
+
 
     def _inner_products(
         self, frequency_domain_strain, signal, power_spectral_density, 
@@ -74,48 +84,6 @@ class GlitchIncludedGravitationalWaveTransient(GravitationalWaveTransient):
 
         return inner_products
 
-    def _frequency_domain_glitch_waveform(self, interferometer):
-
-        wavelet_Q = (
-            2 * np.pi * self.parameters["wavelet_f0"] * self.parameters["wavelet_tau"]
-        )
-
-        psd_at_f0 = (
-            interferometer.power_spectral_density.power_spectral_density_interpolated(
-                self.parameters["wavelet_f0"]
-            )
-        )
-
-        wavelet_A = self.parameters["wavelet_snr"] * np.sqrt(
-            np.sqrt(8 * np.pi) * self.parameters["wavelet_f0"] * psd_at_f0 / wavelet_Q
-        )
-
-        amplitude = (
-            np.sqrt(np.pi / 4)
-            * wavelet_A
-            * self.parameters["wavelet_tau"]
-            * np.exp(
-                   -(
-                        np.pi
-                        * self.parameters["wavelet_tau"]
-                        * (interferometer.frequency_array
-                            - self.parameters["wavelet_f0"]
-                        )
-                    )** 2
-
-            )
-        )
-
-        wave = np.exp(
-            2
-            * np.pi
-            * 1j
-            * self.parameters["wavelet_t0"]
-            * (interferometer.frequency_array - self.parameters["wavelet_f0"])
-        )
-
-        return amplitude * wave
-
     def _glitch_inclusive_likelihood(self, waveform_polarizations):
 
         d_inner_d = 0.0
@@ -123,18 +91,21 @@ class GlitchIncludedGravitationalWaveTransient(GravitationalWaveTransient):
         h_inner_h = 0.0
 
         for interferometer in self.interferometers:
+
             signal = self._compute_full_waveform(
                 signal_polarizations=waveform_polarizations,
                 interferometer=interferometer,
             )
 
-            if interferometer.name == self.ifo_with_glitch:
+            ifo_data = interferometer.frequency_domain_strain
+
+            if interferometer.name in self.ifos_with_glitch:
                 glitch_waveform = self._frequency_domain_glitch_waveform(interferometer)
-                signal = signal + glitch_waveform
+                ifo_data += glitch_waveform
+
 
             inner_products = self._inner_products(
-                interferometer.frequency_domain_strain,
-                signal,
+                ifo_data, signal,
                 interferometer.power_spectral_density_array,
             )
 
@@ -142,11 +113,14 @@ class GlitchIncludedGravitationalWaveTransient(GravitationalWaveTransient):
             d_inner_h += inner_products["d_inner_h"]
             h_inner_h += inner_products["h_inner_h"]
 
-        log_likelihood_ratio = -0.5 * (h_inner_h - 2 * np.real(d_inner_h))
+
+
+        if self.distance_marginalization:
+            log_likelihood_ratio =  self.distance_marginalized_likelihood(d_inner_h, h_inner_h)  
+        else:
+            log_likelihood_ratio = -0.5 * (h_inner_h - 2 * np.real(d_inner_h))
 
         return log_likelihood_ratio
-
-
 
     def log_likelihood(self):
 
@@ -165,3 +139,79 @@ class GlitchIncludedGravitationalWaveTransient(GravitationalWaveTransient):
         noise_log_likelihood = self.noise_log_likelihood()
 
         return log_likelihood_ratio + noise_log_likelihood
+
+
+    ############# ------- Distance marginalization code ----------------
+
+    def distance_marginalized_likelihood(self, d_inner_h, h_inner_h):
+        d_inner_h_ref, h_inner_h_ref = self._setup_rho(
+            d_inner_h, h_inner_h)
+        if self.phase_marginalization:
+            d_inner_h_ref = np.abs(d_inner_h_ref)
+        else:
+            d_inner_h_ref = np.real(d_inner_h_ref)
+
+        return self._interp_dist_margd_loglikelihood(
+            d_inner_h_ref, h_inner_h_ref, grid=False)
+
+
+
+    def _setup_distance_marginalization(self, lookup_table=None):
+        if isinstance(lookup_table, str) or lookup_table is None:
+            self.cached_lookup_table_filename = lookup_table
+            lookup_table = self.load_lookup_table(
+                self.cached_lookup_table_filename)
+        if isinstance(lookup_table, dict):
+            if self._test_cached_lookup_table(lookup_table):
+                self._dist_margd_loglikelihood_array = lookup_table[
+                    'lookup_table']
+            else:
+                self._create_lookup_table()
+        else:
+            self._create_lookup_table()
+        self._interp_dist_margd_loglikelihood = BoundedRectBivariateSpline(
+            self._d_inner_h_ref_array, self._optimal_snr_squared_ref_array,
+            self._dist_margd_loglikelihood_array.T, fill_value=-np.inf)
+
+
+
+
+    @property
+    def _d_inner_h_ref_array(self):
+        """ Matched filter snr at fiducial distance of ref_dist Mpc """
+        if self.phase_marginalization:
+            return np.logspace(-5, 10, self._dist_margd_loglikelihood_array.shape[1])
+        else:
+            n_negative = self._dist_margd_loglikelihood_array.shape[1] // 2
+            n_positive = self._dist_margd_loglikelihood_array.shape[1] - n_negative
+            return np.hstack((
+                -np.logspace(3, -3, n_negative), np.logspace(-3, 10, n_positive)
+            ))
+
+
+
+    def _create_lookup_table(self):
+        """ Make the lookup table """
+        from tqdm.auto import tqdm
+        logger.info('Building lookup table for distance marginalisation.')
+
+        self._dist_margd_loglikelihood_array = np.zeros((400, 800))
+        scaling = self._ref_dist / self._distance_array
+        d_inner_h_array_full = np.outer(self._d_inner_h_ref_array, scaling)
+        h_inner_h_array_full = np.outer(self._optimal_snr_squared_ref_array, scaling ** 2)
+        if self.phase_marginalization:
+            d_inner_h_array_full = ln_i0(abs(d_inner_h_array_full))
+        prior_term = self.distance_prior_array * self._delta_distance
+        for ii, optimal_snr_squared_array in tqdm(
+                enumerate(h_inner_h_array_full), total=len(self._optimal_snr_squared_ref_array)
+        ):
+            for jj, d_inner_h_array in enumerate(d_inner_h_array_full):
+                self._dist_margd_loglikelihood_array[ii][jj] = logsumexp(
+                    d_inner_h_array - optimal_snr_squared_array / 2,
+                    b=prior_term
+                )
+        log_norm = logsumexp(
+            0 / self._distance_array, b=self.distance_prior_array * self._delta_distance
+        )
+        self._dist_margd_loglikelihood_array -= log_norm
+        self.cache_lookup_table()
